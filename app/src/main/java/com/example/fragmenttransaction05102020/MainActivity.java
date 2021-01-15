28fd81f00dc8d66d1a72a7659f44ade4daf16022
phatdroid94 <phatdroid94@gmail.com>
2021-01-15 21:13:16 +0700

    menu

--- app/src/main/java/com/example/fragmenttransaction05102020/MainActivity.java
+++ app/src/main/java/com/example/fragmenttransaction05102020/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.fragmenttransaction05102020;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Fragment Transaction");
+
     }
 }
